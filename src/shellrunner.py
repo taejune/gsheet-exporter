fe6430168d492f3bbfffe11bdd7bb183317fbc02
Azssi <taejune@users.noreply.github.com>
2022-03-11 16:59:15 +0900

    [fix] Capture stderr only

--- src/shellrunner.py
+++ src/shellrunner.py
@@ -4,7 +4,23 @@ class Command:
     def run(self, cmd):
         try:
             # print(cmd)
-            subprocess.run(cmd, check=True, shell=True, capture_output=True).check_returncode()
+            subprocess.run(cmd, check=True, shell=True, stderr=subprocess.PIPE).check_returncode()
             return (True, '')
         except subprocess.SubprocessError as e:
             return (False, str(e.stderr, 'utf-8'))
+
+
+def main():
+    print('test...')
+    cmd = Command()
+
+    ok, msg = cmd.run('ls -al')
+    if not ok:
+        print(msg)
+
+    ok, msg = cmd.run('ls aaa')
+    if not ok:
+        print(msg)
+
+if __name__ == '__main__':
+    main()
